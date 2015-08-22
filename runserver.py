4ffc57bc07d0a477b61ffd59a6c824099041a38b
KrisPlunkett <kris.j.plunkett@icloud.com>
2015-08-22 09:35:55 -0500

    add python live reload aka debug and user route

--- runserver.py
+++ runserver.py
@@ -10,23 +10,13 @@ app = Flask(__name__)
 def home():
     return "Electrode. Powered by Python."
 
-# Another route would look like this:
-# @app.route("/hello")
-# def hello():
-#   return "Howdy!"
-
-# Andy's first route. gonna be sweet
-# I hate pound sign commenting
-# I love //
-# this is mildly similar to PHP's symfony
-# UPDATE: I will figure out how to render a template
 @app.route("/Andy")
 def Andy():
 	return render_template('index.html')
 
-#Still not working********
-
-
+@app.route("/user/<username>")
+def user(username):
+    return "Hello user %s" % username
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=4040)
+    app.run(host='127.0.0.1', port=4040, debug=True)
